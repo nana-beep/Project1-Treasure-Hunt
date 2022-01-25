60ba6d964b618d9b3e6ed8e0982e5dec7f1c6da1
Jina Kim <59896914+nana-beep@users.noreply.github.com>
2022-01-24 23:36:01 -0500

    Add files via upload

---
+++ project1.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <getopt.h>
+#include <sstream>
+#include <list>
+#include "project1.h"
+
+//command line stuff
+
+
+void Map_init(Island& island, Map& map) {
+
+}
+
+
+void Map_make(Island& island, Map& map, char character) {
+
+}
+
+void Valid_character(char character) {
+
+}
